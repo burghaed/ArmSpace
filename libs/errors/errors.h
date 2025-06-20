44da96dafe4139272b6b1b9973c9c69eb34be05d
Eduard Burghardt <eduard.burghardt@gmail.com>
2025-06-20 12:29:55 +0200

    Refactoring: changed architecture corresponding to the srs document

---
+++ libs/errors/errors.h
@@ -0,0 +1,42 @@
+//
+//  Errors.h
+//  ArmSpace
+//
+//  Created by Eduardt Burghardt on 17.06.25.
+//
+
+#pragma once
+#include <array>
+#include <string>
+
+namespace Errors {
+enum class ERROR : unsigned short {
+  ERR001,
+  ERR002,
+  ERR003,
+  ERR004,
+  ERR005,
+  ERR006,
+  ERR007,
+  ERR008,
+  ERR009,
+  ERR0010,
+  ERR011,
+  NUM_ERR
+};
+
+using error_table_t =
+    std::array<std::string, static_cast<size_t>(ERROR::NUM_ERR)>;
+
+error_table_t error_table = {"Invalid Arm Length",
+                             "Disconected Joint",
+                             "Malformed JSON",
+                             "Motor Data Missing",
+                             "Unsupported Joint Count",
+                             "Invalid Rotation Axis",
+                             "Quternion Calculation Failed",
+                             "Tick Calculation Overflow",
+                             "No JSON file specified",
+                             "Cannot open file",
+                             "JSON Parsing Error"};
+} // namespace Errors
---
+++ libs/validator/robotor_arm_validator.cpp
@@ -0,0 +1,12 @@
+//
+//  robotor_arm_validator.cpp
+//  ArmSpace
+//
+//  Created by Eduardt Burghardt on 17.06.25.
+//
+
+#include "robotor_arm_validator.h"
+
+namespace JSON {
+void RobotorArmValidator::validate_scheme(const nlohmann::json &input) {}
+} // namespace JSON
